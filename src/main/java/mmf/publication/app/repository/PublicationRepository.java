cea2f510aece05c107b526f1c4cc07cf9322e97e
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-03-02 21:53:50 +0330

    refactoring of interface PublicationRepository

--- src/main/java/mmf/publication/app/repository/PublicationRepository.java
+++ src/main/java/mmf/publication/app/repository/PublicationRepository.java
@@ -1,13 +1,8 @@
 package mmf.publication.app.repository;
 
 import mmf.publication.app.entity.Publication;
-import mmf.publication.app.enums.PublicationStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PublicationRepository extends JpaRepository<Publication, Long> {
-    Page<Publication> findAll(Pageable pageable);
-
-    Page<Publication> findByStatus(PublicationStatus status, Pageable pageable);
+public interface PublicationRepository extends JpaRepository<Publication, Long>, JpaSpecificationExecutor<Publication> {
 }
