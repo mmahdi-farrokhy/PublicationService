f46a15ed4f971a7032fbbb890e9b26dae72f20a0
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-03-03 14:23:41 -0800

    added abstract method getPublication() to interface IPublicationService

--- src/main/java/mmf/publication/app/service/IPublicationService.java
+++ src/main/java/mmf/publication/app/service/IPublicationService.java
@@ -4,6 +4,7 @@ import mmf.publication.app.dto.PublicationDTO;
 import mmf.publication.app.dto.PublicationRequest;
 import mmf.publication.app.enums.PublicationStatus;
 import mmf.publication.app.enums.PublicationType;
+import mmf.publication.app.exceptions.PublicationNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -25,5 +26,7 @@ public interface IPublicationService {
 
     void incrementViewCount(Long id);
 
-    Optional<Map<String, Integer>> findFrequentWordsOfPublication(Long id);
+    Optional<Map<String, Integer>> findFrequentWordsOfPublication(String publicationDescription);
+
+    PublicationDTO getPublication(Long id) throws PublicationNotFoundException;
 }
