1fcc4c458206b5b73a7fb8652224826dac08de79
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-03-03 17:21:12 -0800

    created AppUserRepository

---
+++ src/main/java/mmf/publication/app/repository/AppUserRepository.java
@@ -0,0 +1,10 @@
+package mmf.publication.app.repository;
+
+import mmf.publication.app.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByUsername(String username);
+}
