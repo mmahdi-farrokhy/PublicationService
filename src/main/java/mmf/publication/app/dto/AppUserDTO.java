8f78c974c202f3c3291144abe10e5248c688ef05
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-03-03 17:21:23 -0800

    created dto AppUserDTO

---
+++ src/main/java/mmf/publication/app/dto/AppUserDTO.java
@@ -0,0 +1,14 @@
+package mmf.publication.app.dto;
+
+public class AppUserDTO {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
